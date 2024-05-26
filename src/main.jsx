94ead3a7e5c839425dff0bfc73b1181f22cce4ef
Erbol Zhaparov <ae.270718@gmail.com>
2024-05-26 16:15:52 +0600

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import AppRoutes from "./routes/AppRoutes.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>,
